bbba0953f79cfdb049659ba4e684b591078fc144
James Bennett <2009bennettj@gmail.com>
2022-06-08 18:28:55 +0100

    Swiper with sample images

--- app/lib/src/features/home/view/components/prompt_content.dart
+++ app/lib/src/features/home/view/components/prompt_content.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class PromptContent extends StatelessWidget {
@@ -7,6 +8,29 @@ class PromptContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final colors = Theme.of(context).colorScheme;
+
+    final imgUrls = [
+      'http://www.davidalbeck.com/photos/2010/mansfield/i020.jpg',
+      'http://www.davidalbeck.com/photos/2015/pond_april/i011.jpg',
+      'http://www.davidalbeck.com/photos/2014/pond19sept/i008.jpg',
+    ];
+    return Swiper(
+      itemCount: 3,
+      itemBuilder: (context, index) {
+        return Image.network(
+          imgUrls[index],
+          fit: BoxFit.contain,
+        );
+      },
+      pagination: SwiperCustomPagination(
+        builder: (c, config) {
+          return DotSwiperPaginationBuilder(
+                  color: colors.onSurface, activeColor: colors.tertiary)
+              .build(c, config);
+        },
+      ),
+      control: SwiperControl(color: colors.onSurface),
+    );
   }
 }
