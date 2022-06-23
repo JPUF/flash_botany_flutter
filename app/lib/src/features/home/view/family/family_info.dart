81a889ee1645bf6e49e78cdf86f70fe747e2c75d
James Bennett <2009bennettj@gmail.com>
2022-06-23 18:45:31 +0100

    Images in FamilyInfo

--- app/lib/src/features/home/view/family/family_info.dart
+++ app/lib/src/features/home/view/family/family_info.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -25,6 +26,8 @@ class FamilyInfo extends StatelessWidget {
           const SizedBox(height: 32),
           Text(family.description, style: context.bodyMedium),
           const SizedBox(height: 32),
+          FamilyInfoImages(urls: family.exampleUrls),
+          const SizedBox(height: 32),
         ],
       ),
     );
@@ -50,6 +53,26 @@ class FamilyInfo extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class FamilyInfoImages extends StatelessWidget {
+  const FamilyInfoImages({Key? key, required this.urls}) : super(key: key);
+
+  final List<String> urls;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      separatorBuilder: (_, __) => const SizedBox(height: 16),
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: urls.length,
+      itemBuilder: (_, i) => ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          child: CachedNetworkImage(imageUrl: urls[i])),
     );
   }
 }
