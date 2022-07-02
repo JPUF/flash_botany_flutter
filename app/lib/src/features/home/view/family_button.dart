636c212c0ec2565b44d3e0c9c07e75afa804d0d1
James Bennett <2009bennettj@gmail.com>
2022-07-02 10:04:19 +0100

    Refactor new widgets

--- app/lib/src/features/home/view/family_button.dart
+++ app/lib/src/features/home/view/family_button.dart
@@ -13,29 +13,52 @@ class FamilyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
     return InkWell(
       onTap: () {},
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-        child: Column(
-          children: [
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: colors.onSecondary,
-                  radius: 50,
-                ),
-                CircleAvatar(
-                    radius: 42, foregroundImage: AssetImage(family.assetImgPath)),
-              ],
-            ),
-            Text(family.latinName,
-                style: context.headlineSmall?.apply(fontStyle: FontStyle.italic))
-          ],
-        ),
+        child: FamilyButtonForeground(family: family),
       ),
+    );
+  }
+}
+
+class FamilyButtonForeground extends StatelessWidget {
+  const FamilyButtonForeground({Key? key, required this.family})
+      : super(key: key);
+
+  final Family family;
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = context.headlineSmall?.apply(fontStyle: FontStyle.italic);
+    return Column(
+      children: [
+        BorderedCircleImage(family: family),
+        Text(family.latinName, style: textStyle),
+      ],
+    );
+  }
+}
+
+class BorderedCircleImage extends StatelessWidget {
+  const BorderedCircleImage({Key? key, required this.family}) : super(key: key);
+
+  final Family family;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: colors.onSecondary,
+          radius: 50,
+        ),
+        CircleAvatar(
+            radius: 42, foregroundImage: AssetImage(family.assetImgPath)),
+      ],
     );
   }
 }
