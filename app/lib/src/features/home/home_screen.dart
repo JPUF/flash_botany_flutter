33a4f854255aed1c44475654ea193f8012270a69
James Bennett <2009bennettj@gmail.com>
2022-07-13 19:20:23 +0100

    full screen scrollbar

--- app/lib/src/features/home/home_screen.dart
+++ app/lib/src/features/home/home_screen.dart
@@ -4,8 +4,6 @@ import 'package:go_router/go_router.dart';
 import '../../shared/destinations.dart';
 import '../../shared/extensions.dart';
 import '../../shared/models/family.dart';
-import '../../shared/strings.dart';
-import '../../shared/views/brightness_toggle.dart';
 import '../../shared/views/custom_app_bar.dart';
 import 'components/family_button.dart';
 import 'components/hero_quiz_button.dart';
@@ -18,14 +16,11 @@ class HomeScreen extends StatelessWidget {
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
         appBar: const CustomAppBar(showBackButton: false),
-        body: SizedBox.expand(
-          child: FractionallySizedBox(
-            widthFactor: constraints.isMobile ? 1 : 0.5,
-            child: const SizedBox.expand(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                child: HomeScreenContent(),
-              ),
+        body: Scrollbar(
+          child: SizedBox.expand(
+            child: FractionallySizedBox(
+              widthFactor: constraints.isMobile ? 1 : 0.5,
+              child: const SizedBox.expand(child: HomeScreenContent()),
             ),
           ),
         ),
@@ -40,59 +35,68 @@ class HomeScreenContent extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext c) {
-    return ListView(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      children: [
-        HeroQuizButton(onTap: () {
-          GoRouter.of(c).go(Destination.quiz.path);
-        }),
-        const SizedBox(height: 32),
-        TextField(
-          style: c.bodyMedium,
-          decoration: const InputDecoration(
-              suffixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(borderSide: BorderSide(width: 0.5)),
-              hintText: 'Family search:'),
-        ),
-        const SizedBox(height: 16),
-        Table(
-          children: [
-            TableRow(children: [
-              FamilyButton(
-                family: Family.asteraceae,
-                onTap: () => _navigateToFamilyScreen(c, Family.asteraceae),
-              ),
-              FamilyButton(
-                family: Family.apiaceae,
-                onTap: () => _navigateToFamilyScreen(c, Family.apiaceae),
-              ),
-            ]),
-            TableRow(children: [
-              FamilyButton(
-                family: Family.boraginaceae,
-                onTap: () => _navigateToFamilyScreen(c, Family.boraginaceae),
-              ),
-              FamilyButton(
-                family: Family.brassicaceae,
-                onTap: () => _navigateToFamilyScreen(c, Family.brassicaceae),
-              ),
-            ]),
-            TableRow(children: [
-              FamilyButton(
-                family: Family.caryophyllaceae,
-                onTap: () => _navigateToFamilyScreen(c, Family.caryophyllaceae),
-              ),
-              FamilyButton(
-                family: Family.lamiaceae,
-                onTap: () => _navigateToFamilyScreen(c, Family.lamiaceae),
-              ),
-            ]),
-          ],
-        ),
-        const SizedBox(height: 32),
-      ],
+  Widget build(BuildContext context) {
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        children: [
+          HeroQuizButton(onTap: () {
+            GoRouter.of(context).go(Destination.quiz.path);
+          }),
+          const SizedBox(height: 32),
+          TextField(
+            style: context.bodyMedium,
+            decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(borderSide: BorderSide(width: 0.5)),
+                hintText: 'Family search:'),
+          ),
+          const SizedBox(height: 16),
+          Table(
+            children: [
+              TableRow(children: [
+                FamilyButton(
+                  family: Family.asteraceae,
+                  onTap: () =>
+                      _navigateToFamilyScreen(context, Family.asteraceae),
+                ),
+                FamilyButton(
+                  family: Family.apiaceae,
+                  onTap: () =>
+                      _navigateToFamilyScreen(context, Family.apiaceae),
+                ),
+              ]),
+              TableRow(children: [
+                FamilyButton(
+                  family: Family.boraginaceae,
+                  onTap: () =>
+                      _navigateToFamilyScreen(context, Family.boraginaceae),
+                ),
+                FamilyButton(
+                  family: Family.brassicaceae,
+                  onTap: () =>
+                      _navigateToFamilyScreen(context, Family.brassicaceae),
+                ),
+              ]),
+              TableRow(children: [
+                FamilyButton(
+                  family: Family.caryophyllaceae,
+                  onTap: () =>
+                      _navigateToFamilyScreen(context, Family.caryophyllaceae),
+                ),
+                FamilyButton(
+                  family: Family.lamiaceae,
+                  onTap: () =>
+                      _navigateToFamilyScreen(context, Family.lamiaceae),
+                ),
+              ]),
+            ],
+          ),
+          const SizedBox(height: 32),
+        ],
+      ),
     );
   }
 
