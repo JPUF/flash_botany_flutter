f6388be262233c5d16cc0102c8131a50cddb9ac8
James Bennett <2009bennettj@gmail.com>
2022-09-07 14:56:00 +0200

    Attributions with static text

--- app/lib/src/features/quiz/view/flash/prompt_content.dart
+++ app/lib/src/features/quiz/view/flash/prompt_content.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../shared/blocs/prompt_bloc.dart';
+import '../../../../shared/extensions.dart';
 
 class PromptContent extends StatefulWidget {
   const PromptContent({
@@ -25,30 +26,101 @@ class _PromptContentState extends State<PromptContent> {
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
 
-    return Swiper(
-      itemCount: 3,
-      itemBuilder: (context, index) {
-        return BlocBuilder<PromptBloc, PromptState>(
-          builder: (context, state) {
-            final urls = state.promptSpecies?.imageUrls ?? [];
-            if (urls.isNotEmpty) {
-              return PromptNetworkImage(imgUrls: urls, index: index);
-            } else {
-              return const SizedLoadSpinner();
-            }
+    return Stack(
+      alignment: AlignmentDirectional.bottomEnd,
+      children: [
+        Swiper(
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return BlocBuilder<PromptBloc, PromptState>(
+              builder: (context, state) {
+                final urls = state.promptSpecies?.imageUrls ?? [];
+                if (urls.isNotEmpty) {
+                  return PromptNetworkImage(imgUrls: urls, index: index);
+                } else {
+                  return const SizedLoadSpinner();
+                }
+              },
+            );
           },
-        );
-      },
-      pagination: SwiperPagination(
-          alignment: Alignment.bottomCenter,
-          builder: SwiperCustomPagination(builder: (c, config) {
-            return const DotSwiperPaginationBuilder(
-                    color: Colors.white, activeColor: Colors.white)
-                .build(c, config);
-          })),
-      control: SwiperControl(
-          color: colors.onSurface, padding: const EdgeInsets.only(left: 8)),
-      indicatorLayout: PageIndicatorLayout.SCALE,
+          pagination: SwiperPagination(
+              alignment: Alignment.bottomCenter,
+              builder: SwiperCustomPagination(builder: (c, config) {
+                return const DotSwiperPaginationBuilder(
+                        color: Colors.white, activeColor: Colors.white)
+                    .build(c, config);
+              })),
+          control: SwiperControl(
+              color: colors.onSurface, padding: const EdgeInsets.only(left: 8)),
+          indicatorLayout: PageIndicatorLayout.SCALE,
+        ),
+        const AttributionContainer(),
+      ],
+    );
+  }
+}
+
+class AttributionContainer extends StatefulWidget {
+  const AttributionContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AttributionContainer> createState() => _AttributionContainerState();
+}
+
+class _AttributionContainerState extends State<AttributionContainer> {
+
+  bool showAttribution = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const SizedBox(width: 50),
+          Visibility(
+            visible: showAttribution,
+            child: Expanded(
+              child: attributionText(
+                colors.onInverseSurface,
+                context.attributionLabel,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() => showAttribution = !showAttribution);
+            },
+            child: Container(
+              width: 50,
+              height: 50,
+              alignment: AlignmentDirectional.bottomEnd,
+              padding: const EdgeInsets.all(8),
+              child: Icon(
+                Icons.info_outline,
+                size: 20,
+                color: colors.surfaceVariant,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget attributionText(Color bgColor, TextStyle? fgStyle) {
+    return Card(
+      elevation: 4,
+      color: bgColor,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text('Credit: Rob Routledge, Sault College, Bugwood.org', style: fgStyle),
+      ),
     );
   }
 }
