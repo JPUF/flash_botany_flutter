2f935ca4ef0cf9777f24733ad0bc1102dfa7d39a
James Bennett <2009bennettj@gmail.com>
2022-09-12 15:13:04 +0200

    brassicaceae species

--- app/lib/src/shared/data/brassicaceae_data.dart
+++ app/lib/src/shared/data/brassicaceae_data.dart
@@ -25,5 +25,34 @@ class BrassicaceaeData {
         AttributedUrl(_base + 'allilaria_petiolata_3.jpg', '© Сергей (CC-BY-NC)'),
       ],
     ),
+    const Species(
+      latinName: 'Capsella bursa-pastoris',
+      commonName: 'Shepherd’s-Purse',
+      family: Family.brassicaceae,
+      images: [
+        AttributedUrl(_base + 'capsella_bursapastoris_1.jpg', '© svetlana-bogdanovich (CC-BY-NC)'),
+        AttributedUrl(_base + 'capsella_bursapastoris_2.jpg', '© aitanaluis (CC-BY-NC)'),
+        AttributedUrl(_base + 'capsella_bursapastoris_3.jpg', '© Andreas Stiller (CC-BY-NC)'),
+      ],
+    ),
+    const Species(
+      latinName: 'Lunaria annua',
+      commonName: 'Annual Honesty',
+      family: Family.brassicaceae,
+      images: [
+        AttributedUrl(_base + 'lunaria_annua_1.jpg', '© Thanasis Papanikolaou (CC-BY-NC)'),
+        AttributedUrl(_base + 'lunaria_annua_2.jpg', '© olekje (CC-BY-NC)'),
+      ],
+    ),
+    const Species(
+      latinName: 'Cakile maritima',
+      commonName: 'Searocket',
+      family: Family.brassicaceae,
+      images: [
+        AttributedUrl(_base + 'cakile_maritima_1.jpg', '© rosagruen, (CC-BY-NC)'),
+        AttributedUrl(_base + 'cakile_maritima_2.jpg', '© Γιώργος Παντάκης, (CC-BY-NC)'),
+        AttributedUrl(_base + 'cakile_maritima_3.jpg', '© lauribanane, (CC-BY-NC)'),
+      ],
+    ),
   ];
 }
