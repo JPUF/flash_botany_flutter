963525f53990edc348cd7cbdbc8803fa475ada69
James Bennett <2009bennettj@gmail.com>
2022-09-15 13:33:52 +0200

    reset family info tab on new prompt

--- app/lib/src/features/quiz/view/family/family_sheet_scaffold.dart
+++ app/lib/src/features/quiz/view/family/family_sheet_scaffold.dart
@@ -5,13 +5,27 @@ import '../../../../shared/blocs/prompt_bloc.dart';
 import '../flash/prompt_content.dart';
 import 'family_info.dart';
 
-class FamilySheetContent extends StatelessWidget {
+class FamilySheetContent extends StatefulWidget {
   const FamilySheetContent({
     Key? key,
     this.sheetExpansionPercent,
   }) : super(key: key);
 
   final double? sheetExpansionPercent;
+
+  @override
+  State<FamilySheetContent> createState() => _FamilySheetContentState();
+}
+
+class _FamilySheetContentState extends State<FamilySheetContent>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 4, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +37,38 @@ class FamilySheetContent extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: colors.surfaceVariant,
           flexibleSpace: SafeArea(
-            child: BlocBuilder<PromptBloc, PromptState>(
-              builder: (context, state) {
-                final families = state.familyOptions;
-                return families != null && families.length == 4
-                    ? TabBar(
-                        isScrollable: true,
-                        tabs: families.map((f) => Tab(text: f.latinName)).toList(),
-                      )
-                    : Container();
+            child: BlocListener<PromptBloc, PromptState>(
+              listenWhen: (previous, current) {
+                return previous.promptSpecies != current.promptSpecies;
               },
+              listener: (_, __) {
+                _tabController.index = 0;
+              },
+              child: BlocBuilder<PromptBloc, PromptState>(
+                builder: (context, state) {
+                  final families = state.familyOptions;
+                  return families != null && families.length == 4
+                      ? TabBar(
+                          controller: _tabController,
+                          isScrollable: true,
+                          tabs: families
+                              .map((f) => Tab(text: f.latinName))
+                              .toList(),
+                        )
+                      : Container();
+                },
+              ),
             ),
           ),
         ),
         body: Opacity(
-          opacity: getOpacityPercentage(sheetExpansionPercent ?? 1),
+          opacity: getOpacityPercentage(widget.sheetExpansionPercent ?? 1),
           child: BlocBuilder<PromptBloc, PromptState>(
             builder: (context, state) {
               final families = state.familyOptions;
               return families != null && families.length == 4
                   ? TabBarView(
+                      controller: _tabController,
                       physics: const NeverScrollableScrollPhysics(),
                       children:
                           families.map((f) => FamilyInfo(family: f)).toList(),
