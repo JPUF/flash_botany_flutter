38018ad835eb052bbe26c7d8de9ce870b7f29285
James Bennett <2009bennettj@gmail.com>
2022-09-20 14:22:14 +0200

    Feedback sounds

--- app/lib/src/shared/blocs/prompt_bloc.dart
+++ app/lib/src/shared/blocs/prompt_bloc.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:soundpool/soundpool.dart';
 
 import '../data/species_data.dart';
 import '../models/family.dart';
@@ -12,7 +14,16 @@ part 'prompt_event.dart';
 part 'prompt_state.dart';
 
 class PromptBloc extends Bloc<PromptEvent, PromptState> {
+  final Soundpool _pool = Soundpool.fromOptions(
+    options: SoundpoolOptions.kDefault,
+  );
+
+  late Future<int> _correctSound;
+  late Future<int> _wrongSound;
+
   PromptBloc() : super(PromptState.initial()) {
+    _correctSound = _loadCorrectSound();
+    _wrongSound = _loadWrongSound();
     on<PromptEvent>(
       (event, emit) => event.map(
         nextPrompt: (event) => _nextPrompt(event, emit),
@@ -32,6 +43,7 @@ class PromptBloc extends Bloc<PromptEvent, PromptState> {
   void _getFeedback(GetFeedback event, Emitter<PromptState> emit) {
     final family = event.selectedFamily;
     final correct = family == state.promptSpecies?.family;
+    _playFeedbackSound(correct);
     emit(state.copyWith(correct: correct));
   }
 
@@ -55,5 +67,23 @@ class PromptBloc extends Bloc<PromptEvent, PromptState> {
       newOptions.add(isCorrect ? correctFamily : remainingFamilies[i]);
     }
     return newOptions;
+  }
+
+  Future<void> _playFeedbackSound(bool correct) async {
+    var id = await _wrongSound;
+    if (correct) {
+      id = await _correctSound;
+    }
+    await _pool.play(id);
+  }
+
+  Future<int> _loadCorrectSound() async {
+    final asset = await rootBundle.load('assets/sounds/sound_correct.m4a');
+    return await _pool.load(asset);
+  }
+
+  Future<int> _loadWrongSound() async {
+    final asset = await rootBundle.load('assets/sounds/sound_wrong.m4a');
+    return await _pool.load(asset);
   }
 }
--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import dynamic_color
 import path_provider_macos
 import shared_preferences_macos
+import soundpool_macos
 import sqflite
 import url_launcher_macos
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
