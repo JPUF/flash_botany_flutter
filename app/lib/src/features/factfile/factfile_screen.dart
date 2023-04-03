f5848a82879eaf3a70085dae4e476d15ada11dee
James Bennett <2009bennettj@gmail.com>
2023-04-03 22:26:43 +0200

    Families on new screen

--- app/lib/src/features/factfile/factfile_screen.dart
+++ app/lib/src/features/factfile/factfile_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/extensions.dart';
+import '../../shared/models/family.dart';
+import '../../shared/strings.dart';
+import '../../shared/util_widgets.dart';
 import '../../shared/views/custom_app_bar.dart';
+import '../home/components/family_button.dart';
 
 class FactfileScreen extends StatelessWidget {
   const FactfileScreen({Key? key}) : super(key: key);
@@ -18,16 +23,85 @@ class FactfileScreen extends StatelessWidget {
   }
 }
 
-class FactfileContent extends StatelessWidget {
+class FactfileContent extends StatefulWidget {
   const FactfileContent({Key? key}) : super(key: key);
 
   @override
+  State<FactfileContent> createState() => _FactfileContentState();
+}
+
+class _FactfileContentState extends State<FactfileContent> {
+  String searchTerm = '';
+
+  @override
   Widget build(BuildContext context) {
+    List<TableRow> rows = _filteredFamilyButtons(searchTerm);
+
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       width: double.infinity,
       alignment: AlignmentDirectional.topCenter,
-      child: const Text("Factfile"),
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          PlatformSized(child: _familySearchField()),
+          const SizedBox(height: 16),
+          PlatformSized(child: Table(children: rows)),
+          const SizedBox(height: 32),
+        ],
+      ),
     );
+  }
+
+  Widget _familySearchField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: TextField(
+        onChanged: (text) {
+          setState(() => searchTerm = text);
+        },
+        style: context.bodyMedium,
+        decoration: InputDecoration(
+            suffixIcon: searchTerm.isEmpty
+                ? const Icon(Icons.search)
+                : IconButton(
+                    icon: const Icon(Icons.clear),
+                    onPressed: () => setState(() => searchTerm = '')),
+            border:
+                const OutlineInputBorder(borderSide: BorderSide(width: 0.5)),
+            hintText: Strings.familySearchHint),
+      ),
+    );
+  }
+
+  List<TableRow> _filteredFamilyButtons(String searchTerm) {
+    List<TableRow> rows = [];
+    final families =
+        searchTerm.isEmpty ? Family.values : _filterFamilies(searchTerm);
+    if (families.isEmpty) {
+      rows.add(TableRow(children: [
+        Text(
+          Strings.familySearchNoResults,
+          style: context.bodyLarge,
+          textAlign: TextAlign.center,
+        )
+      ]));
+    }
+    for (int i = 0; i < families.length; i += 2) {
+      rows.add(TableRow(children: [
+        FamilyButton(family: families[i]),
+        i + 1 < families.length
+            ? FamilyButton(family: families[i + 1])
+            : Container(),
+      ]));
+    }
+    return rows;
+  }
+
+  List<Family> _filterFamilies(String searchTerm) {
+    return Family.values
+        .where(
+            (f) => f.latinName.toLowerCase().contains(searchTerm.toLowerCase()))
+        .toList();
   }
 }
