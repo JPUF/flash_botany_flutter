8133ce9798fc4da58c72aa6e8ff2d64b80b981a3
James Bennett <2009bennettj@gmail.com>
2023-05-07 17:34:46 +0200

    Completed orchid data

--- app/lib/src/shared/data/families/orchidaceae_data.dart
+++ app/lib/src/shared/data/families/orchidaceae_data.dart
@@ -24,6 +24,32 @@ class OrchidaceaeData {
         AttributedUrl(url: _base + 'ophrys_apifera_2.jpg', attribution: '© Dean Zagorac (CC-BY-NC)'),
       ],
     ),
-    //TODO three more orchids.
+    Species(
+      latinName: 'Platanthera bifolia',
+      commonName: 'Lesser Butterfly Orchid',
+      family: Family.orchidaceae,
+      images: [
+        AttributedUrl(url: _base + 'platanthera_bifolia_1.jpg', attribution: '© TOV Muusse (CC-BY-NC)'),
+        AttributedUrl(url: _base + 'platanthera_bifolia_2.jpg', attribution: '© @c.fenoll (CC-BY-NC)'),
+      ],
+    ),
+    Species(
+      latinName: 'Epipactis helleborine',
+      commonName: 'Broad-leaved Helleborine',
+      family: Family.orchidaceae,
+      images: [
+        AttributedUrl(url: _base + 'epipactis_helleborine_1.jpg', attribution: '© François Plourde — Renard frak (CC-BY-NC)'),
+        AttributedUrl(url: _base + 'epipactis_helleborine_2.jpg', attribution: '© Vladimir Bryukhov (CC-BY-NC)'),
+      ],
+    ),
+    Species(
+      latinName: 'Himantoglossum robertianum',
+      commonName: 'Giant Orchid',
+      family: Family.orchidaceae,
+      images: [
+        AttributedUrl(url: _base + 'himantoglossum_robertianum_1.jpg', attribution: '© Giacomo Gola (CC-BY-NC)'),
+        AttributedUrl(url: _base + 'himantoglossum_robertianum_2.jpg', attribution: '© Mario Bassini (CC-BY)'),
+      ],
+    ),
   ];
 }
